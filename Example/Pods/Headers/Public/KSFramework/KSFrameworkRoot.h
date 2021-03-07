af850a7377bd25a180ef7db64fcf61c232404443
KonaCurrents <scott@konacurrents.com>
2021-03-07 15:58:07 -0800

    done

---
+++ Example/Pods/Headers/Public/KSFramework/KSFrameworkRoot.h
@@ -0,0 +1,1 @@
+../../../../../KSFramework/Classes/KSFrameworkRoot.h
---
+++ Example/Pods/Target Support Files/KSFramework/KSFramework-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_KSFramework : NSObject
+@end
+@implementation PodsDummy_KSFramework
+@end
